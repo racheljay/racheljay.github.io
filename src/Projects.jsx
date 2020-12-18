30d52a290bca10faf2cdbbfbd5001f88bdf0b17a
Rachel <racheljay42@gmail.com>
2020-12-18 15:57:30 -0500

    projects now mapping from json file

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,153 +1,38 @@
 import React from 'react';
+import projects from './data/projects.json';
 
 function Projects(props) {
+  console.log(projects.projects)
   return (
     <div className="container mt-5 pt-3">
 
       <div className="row mt-2 mb-2">
         <h1 className="col">
           Here's what I've been working on:
-                </h1>
+          </h1>
       </div>
 
-      <div className="card-deck mb-4">
+      <div className="row">
+        {projects.projects.map((item, index) => {
+          return (
+            <div className="col-md-4 mb-4" key={index}>
+              <a href={item.link} target="_blank">
 
+                <div className="card" >
+                  <img className="card-img-top" src={item.img} alt="Card image cap" />
+                  <div className="card-body">
+                    <h5 className="card-title">{item.name}</h5>
+                    <p className="card-text">{item.week}</p>
+                  </div>
+                </div>
+              </a>
 
-        <div className="card bg-light">
-          <a href="https://racheljay.github.io/digital-clock/" target="_blank">
-            <img className="card-img-top" src="./img/clock-screenshot600.png" alt="Card image cap" />
-            <div className="card-body">
-              <h5 className="card-title">Digital Clock</h5>
-              <p className="card-text"></p>
-              <p className="card-text"><small className="text-muted">Prework: Week Four</small></p>
             </div>
-          </a>
-        </div>
-
-
-
-        <div className="card">
-          <a href="https://racheljay.github.io/countdown-timer/" target="_blank">
-            <img className="card-img-top" src="img/countdown-screenshot600.png" alt="Card image cap" />
-            <div className="card-body">
-              <h5 className="card-title">Countdown Timer</h5>
-              <p className="card-text"></p>
-              <p className="card-text"><small className="text-muted">Prework: Week Four</small></p>
-            </div>
-          </a>
-        </div>
-
-
-
-        <div className="card bg-light">
-          <a href="https://racheljay.github.io/campaign-components/" target="_blank">
-            <img className="card-img-top" src="img/campaign-screenshot600.png" alt="Card image cap" />
-            <div className="card-body">
-              <h5 className="card-title">Campaign Components</h5>
-              <p className="card-text"></p>
-              <p className="card-text"><small className="text-muted">Frontend: Week 1</small></p>
-            </div>
-          </a>
-        </div>
-
-
-
-
+          )
+        })}
       </div>
 
-      {/* <!--Row 2--> */}
-
-      <div className="card-deck mb-4">
-
-
-        <div className="card">
-          <a href="https://racheljay.github.io/mind-reader/" target="_blank">
-            <img className="card-img-top" src="img/mindreader600.png" alt="Card image cap" />
-            <div className="card-body">
-              <h5 className="card-title">Mind Reader</h5>
-              <p className="card-text"></p>
-              <p className="card-text"><small className="text-muted">Frontend: Week 1</small></p>
-            </div>
-          </a>
-        </div>
-
-
-
-        <div className="card bg-light">
-          <a href="https://racheljay.github.io/async-weather-app/" target="_blank">
-            <img className="card-img-top" src="img/weather600.png" alt="Card image cap" />
-            <div className="card-body">
-              <h5 className="card-title">Weather App</h5>
-              <p className="card-text"></p>
-              <p className="card-text"><small className="text-muted">Frontend: Week 2</small></p>
-            </div>
-          </a>
-        </div>
-
-
-
-        <div className="card ">
-          <a href="https://racheljay.github.io/tic-tac-toe" target="_blank">
-            <img className="card-img-top" src="img/tictac600.png" alt="Card image cap" />
-            <div className="card-body">
-              <h5 className="card-title">Tic-Tac-Toe</h5>
-              <p className="card-text"></p>
-              <p className="card-text"><small className="text-muted">Frontend: Week 3</small></p>
-            </div>
-          </a>
-        </div>
-
-
-
-
-      </div>
-
-      {/* Row Three */}
-
-      <div className="card-deck mb-4">
-
-
-        <div className="card bg-light">
-          <a href="https://fishing-hooks-f357a.web.app/" target="_blank">
-            <img className="card-img-top" src="./img/fishinghooks.png" alt="Card image cap" />
-            <div className="card-body">
-              <h5 className="card-title">Fishing Hooks</h5>
-              <p className="card-text"></p>
-              <p className="card-text"><small className="text-muted"></small></p>
-            </div>
-          </a>
-        </div>
-
-
-
-        <div className="card">
-          <a href="https://react-restaurant-73e80.web.app/" target="_blank">
-            <img className="card-img-top" src="img/reactrestaurant.png" alt="Card image cap" />
-            <div className="card-body">
-              <h5 className="card-title">React Restaurant</h5>
-              <p className="card-text"></p>
-              <p className="card-text"><small className="text-muted"></small></p>
-            </div>
-          </a>
-        </div>
-
-
-
-        <div className="card bg-light">
-          <a href="https://knit-keeper-30b32.web.app/" target="_blank">
-            <img className="card-img-top" src="img/knitkeeper.png" alt="Card image cap" />
-            <div className="card-body">
-              <h5 className="card-title">Knit Keeper</h5>
-              <p className="card-text"></p>
-              <p className="card-text"><small className="text-muted">Final Project</small></p>
-            </div>
-          </a>
-        </div>
-
-
-
-
-      </div>
+      
 
     </div>
   )
