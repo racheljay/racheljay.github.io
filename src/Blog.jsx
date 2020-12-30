fc4cf113eae9b62107fd55703a0abb61130f3422
Rachel <racheljay42@gmail.com>
2020-12-30 18:04:23 -0500

    finished out bootcamp blog

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import  blogPosts from './data/blog-posts.json';
+import blogPosts from './data/blog-posts.json';
 
 function Blog(props) {
-console.log(blogPosts)
+    console.log(blogPosts)
     const posts = blogPosts.posts
     return (
         <div className="container">
-        <h1>Rachel's Code Blog</h1>
-        {posts.map((item, index) => {
-            return (
-                <>
-            <h2 key={item}>{item.week}</h2>
-            <h3 key={item}>{item.date}</h3>
-            {item.content.map((item, index) => {
-               return <p key={item}>{item}</p>
+            <h1>Rachel's Code Blog</h1>
+            {posts.map((item, index) => {
+                return (
+                    <div key={index}>
+                        <h2>{item.week}</h2>
+                        <h3>{item.date}</h3>
+                        {item.content.map((item, index) => {
+                            return <p key={index}>{item}</p>
+                        })}
+                        <hr />
+                    </div>
+                )
             })}
-            <hr/>
-            </>
-            )
-        })}
         </div>
     )
 }
