df45a0429440c86c887c23e0b820bbaef51f35e1
Rachel <racheljay42@gmail.com>
2021-01-03 21:45:42 -0500

    added post work blog post

--- src/components/NewBlog.jsx
+++ src/components/NewBlog.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
+import newBlog from '../data/post-blogs.json'
 
 function NewBlog() {
+    const posts = newBlog.posts
     return(
         <div className="container mt-5">
             <div className="jumbotron jumbotron-fluid mt-4">
                 <div className="container">
                     <h1 className="display-4">Post Work:</h1>
-                    <p className="lead">Check back soon for blog posts on job searching, project progress, and more!</p>
+                    <p className="lead">What have I been up to since bootcamp?</p>
                 </div>
             </div>
 
+            {posts.map((item, index) => {
+                return (
+                    <div key={index}>
+                        <h2>{item.title}</h2>
+                        <h3>{item.date}</h3>
+                        {item.content.map((item, index) => {
+                            return <p key={index}>{item}</p>
+                        })}
+                        <hr />
+                    </div>
+                )
+            })}
            
         </div>
     )
