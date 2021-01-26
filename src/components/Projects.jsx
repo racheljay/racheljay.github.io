cc953870ba592cbd51fc879ef4f9bc20592828ae
Rachel <racheljay42@gmail.com>
2021-01-26 17:05:40 -0500

    added github repos to json

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,24 +1,26 @@
-import React, {useContext, useEffect, useState} from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import projects from '../data/projects.json';
 import AppContext from '../utilities/AppContext';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 
 function Projects(props) {
   console.log(projects.projects)
 
-  const {setCurrentPage, flipped, setFlipped} = useContext(AppContext);
+  const { setCurrentPage, flipped, setFlipped } = useContext(AppContext);
 
   const [content, setContent] = useState(projects.projects)
 
-	useEffect(() => {
+  useEffect(() => {
     setCurrentPage('projects')
-    if(!flipped) {
+    if (!flipped) {
       setContent(content.reverse())
     }
     setFlipped(true)
     console.log(flipped)
 
-	},[])
+  }, [])
   return (
     <div className="container mt-5 pt-3">
 
@@ -32,23 +34,27 @@ function Projects(props) {
         {content.map((item, index) => {
           return (
             <div className="col-md-4 mb-4" key={index}>
-              <a href={item.link} target="_blank">
 
-                <div className="card" >
+              <div className="card" >
+                <a href={item.link} target="_blank">
                   <img className="card-img-top" src={item.img} alt="Card image cap" />
-                  <div className="card-body">
+                </a>
+                <div className="card-body">
+                  <a href={item.link} target="_blank">
+
                     <h5 className="card-title">{item.name}</h5>
                     <p className="card-text">{item.week}</p>
-                  </div>
+                  </a>
+                  <h5 className="card-text"><FontAwesomeIcon icon={faGithub} /></h5>
                 </div>
-              </a>
+              </div>
 
             </div>
           )
         })}
       </div>
 
-      
+
 
     </div>
   )
